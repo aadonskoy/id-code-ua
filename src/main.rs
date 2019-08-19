7d5ec02a65cd0565e453b61a72feda88d4c91446
Alexey Donskoy <alexeydonskoy@MacBook-Pro-0087.local>
2019-08-19 12:10:09 +0300

    Trying to receive vec as a result from threads.

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,26 @@ use scoped_threadpool::Pool;
 fn main() {
     let threads: usize = 100;
     let step: u32 = (100_000_000 - ID_LOWEST) / threads as u32;
+    let mut ress: Vec<Vec<u32>> = Vec::with_capacity(threads + 1);
 
     let mut pool = Pool::new(threads as u32);
-
     pool.scoped(|scoped| {
-        for piece in 0..(threads) {
+        for piece in 0..threads {
             let begin = ID_LOWEST + piece as u32 * step;
             let end = begin + step;
             println!("{}: {} - {}", piece, begin, end);
 
             scoped.execute(move || {
+                let mut r: Vec<u32> = vec![];
                 for id in begin..end {
                     let valid = is_valid(id);
                     if valid {
-                        println!("{}", id);
+                        r.push(id);
                     }
                 }
+                println!("{:?}", r);
             });
         }
     });
+    println!("{:?}", ress);
 }
